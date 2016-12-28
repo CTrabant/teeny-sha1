7d4a33095338ee016b490720215f725ef0047bf1
CTrabant <ctrabant@gmail.com>
2016-12-27 16:17:28 -0800

    Add initial source code, test program and README.md

---
+++ README.md
@@ -0,0 +1,63 @@
+# Teeny SHA-1
+
+A small, single function implementation of the
+[SHA-1](https://en.wikipedia.org/wiki/SHA-1) in straightforward C
+(C99) that generates a result in hexadecimal.  This code is based on
+the SHA-1 implementation in
+[EncryptionLibrary](https://github.com/jinqiangshou/EncryptionLibrary)
+by [@jinqiangshou](https://github.com/jinqiangshou).
+
+If you need a SHA-1 calculation with no external dependencies to embed
+in your project this may be for you.  This implementation does not
+provide an incremental update interface, the entire buffer to be
+hashed must be in memory in a single buffer.  This is also
+approximately an order of magnitude slower than solutions like
+[OpenSSL](https://www.openssl.org/).  If you are doing many, many hash
+calculations on very large buffers and/or need to do chunked
+calculations you may wish to look elsewhere.
+
+## Usage pattern
+
+The expectation is that you'll copy the function into an existing
+project source code file or add the teeny-sha1.c file to your project
+and then add a declaration for the fuction whereever it's needed.
+
+#### The sha1digest() documentation
+```
+/*******************************************************************************
+ * sha1digest: https://github.com/CTrabant/teeny-sha1
+ *
+ * Calculate the SHA-1 value for supplied data buffer and generate a
+ * text representation in hexadecimal.
+ *
+ * Based on https://github.com/jinqiangshou/EncryptionLibrary, credit
+ * goes to @jinqiangshou, all new bugs are mine.
+ *
+ * @input:
+ *    data      -- data to be hashed
+ *    databytes -- bytes in data buffer to be hashed
+ *
+ * @output:
+ *    digest    -- the result in hex, MUST have at least 41 bytes.
+ *
+ * @return: 0 on success and non-zero on error.
+ ******************************************************************************/
+int
+sha1digest(char *digest, const uint8_t *data, size_t databytes)
+```
+
+## MIT License
+
+See the included LICENSE details for more details.  The original was
+MIT licensed and so is this in kind.
+
+## Teeny?
+
+I was looking for a small implementation of SHA-1 while trying to
+avoid dependencies like OpenSSL.  In my case I was writing a WebSocket
+implmenetation in a project not already dependent on OpenSSL or other
+library providing SHA-1 and needed to hash 30-50 bytes of data.  There
+are many "tiny" implementations around, but I found them not-so-tiny
+and nearly always requiring multiple functions.  This function
+provides a SHA-1 in ~150 lines of commented C99 in a single function,
+no fuss.
---
+++ teeny-sha1-test.c
@@ -0,0 +1,199 @@
+/******************************************************************************
+ * A test program for the SHA-1 function in teeny-sha1.c
+ *
+ * The tests are conducted by calculating the SHA-1 values using
+ * sha1digest() and comparing the result to the known digest value.
+ *
+ * Included are some data values and known hashes from
+ * http://www.di-mgt.com.au/sha_testvectors.html, a few examples from
+ * https://en.wikipedia.org/wiki/SHA-1 and the ability to use test
+ * vectors from NIST's National Software Reference Library (NSRL).
+ *
+ * Comand line options:
+ *   -l : Perform a large (1GB) test
+ *   -nsrl <directory>
+ *        Perform tests with vectors provided in:
+ *        http://www.nsrl.nist.gov/testdata/NSRLvectors.zip
+ *        The test vectors must be downloaded and unzipped manually.
+ *
+ * Build test program:
+ *   cc -g -O2 -Wall teeny-sha1.c teeny-sha1-test.c -o teeny-sha1-test
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+/* Declare sha1digest and use directly from teeny-sha1.c */
+extern int sha1digest(char *digest, const uint8_t *data, size_t databytes);
+
+/* Run hash function and print comparison results.
+ * Return 1 on mismatch with known digest and 0 on match. */
+int
+testhash (char *data, size_t databytes, char *knowndigest)
+{
+  char digest[41];
+  char *statusstr;
+  int mismatch;
+
+  /* Generate digest */
+  if (sha1digest (digest, (uint8_t *)data, databytes))
+  {
+    fprintf (stderr, "Error with sha1digest\n");
+    return 1;
+  }
+
+  mismatch = (strcasecmp (digest, knowndigest)) ? 1 : 0;
+  statusstr = (mismatch) ? "does NOT match" : "matches";
+
+  printf ("Known digest:  '%s'  data length: %zu\n", knowndigest, databytes);
+  printf ("Teeny digest:  '%s'  %s\n", digest, statusstr);
+  printf ("\n");
+
+  return mismatch;
+}
+
+int
+main (int argc, char **argv)
+{
+  char *data;
+  char *knowndigest;
+  char *nsrldir = NULL;
+  int failurecount = 0;
+  int largetest = 0;
+  int idx;
+
+  /* Process command line options */
+  for (idx = 0; idx < argc; idx++)
+  {
+    /* A "-l" option turns on the large test */
+    if (!strcmp (argv[idx], "-l"))
+      largetest = 1;
+
+    /* A "-nsrl <directory>" option does testing with the NSRL Sample Vectors subset */
+    if (!strcmp (argv[idx], "-nsrl"))
+    {
+      if ((idx + 1) == argc)
+      {
+        printf ("-nsrl option requires a directory argument\n");
+        return 1;
+      }
+      else
+        nsrldir = argv[idx + 1];
+    }
+  }
+
+  /* Test vectors from http://www.di-mgt.com.au/sha_testvectors.html */
+  data = "abc";
+  knowndigest = "a9993e364706816aba3e25717850c26c9cd0d89d";
+  failurecount += testhash (data, strlen(data), knowndigest);
+
+  data = "";
+  knowndigest = "da39a3ee5e6b4b0d3255bfef95601890afd80709";
+  failurecount += testhash (data, strlen(data), knowndigest);
+
+  data = "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq";
+  knowndigest = "84983e441c3bd26ebaae4aa1f95129e5e54670f1";
+  failurecount += testhash (data, strlen(data), knowndigest);
+
+  data = "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu";
+  knowndigest = "a49b2446a02c645bf419f995b67091253a04a259";
+  failurecount += testhash (data, strlen(data), knowndigest);
+
+  /* One million repetitions of 'a' */
+  data = (char *)malloc (1000000);
+  memset (data, 'a', 1000000);
+  knowndigest = "34aa973cd4c4daa4f61eeb2bdbad27316534016f";
+  failurecount += testhash (data, 1000000, knowndigest);
+  free (data);
+
+  /* Large test (~1GB), 16,777,216 repetitions of base */
+  if (largetest)
+  {
+    char *base = "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmno";
+    size_t baselen = strlen(base);
+    data = (char *) malloc (16777216 * baselen);
+    for (int idx = 0; idx < 16777216; idx++)
+    {
+      memcpy (data + (idx * baselen), base, baselen);
+    }
+
+    knowndigest = "7789f0c9ef7bfc40d93311143dfbe69e2017f592";
+    failurecount += testhash (data, baselen * 16777216, knowndigest);
+    free(data);
+  }
+
+  /* Example hashes from Wikipedia */
+  data = "The quick brown fox jumps over the lazy dog";
+  knowndigest = "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12";
+  failurecount += testhash (data, strlen(data), knowndigest);
+
+  data = "The quick brown fox jumps over the lazy cog";
+  knowndigest = "de9f2c7fd25e1b3afad3e85a0bd17d9b100db4b3";
+  failurecount += testhash (data, strlen(data), knowndigest);
+
+  /* Read test vectors from the NIST NSRL Sample Vectors subset:
+     http://www.nsrl.nist.gov/testdata/
+     http://www.nsrl.nist.gov/testdata/NSRLvectors.zip */
+  if (nsrldir)
+  {
+    FILE *infile;
+    struct stat sb;
+    char *nsrlhashes[200] = {0};
+    char path[100];
+    char line[100];
+    int hashcount = 0;
+    int idx;
+
+    /* Open byte-hashes.sha1 and store known hash values (in hex) */
+    snprintf (path, sizeof(path), "%s/byte-hashes.sha1", nsrldir);
+    if (!(infile = fopen(path, "r")))
+    {
+      printf ("Error opening %s: %s\n", path, strerror(errno));
+      return 1;
+    }
+    while (fgets(line, sizeof(line), infile))
+    {
+      /* Store known hash values, e.g. "DA39A3EE5E6B4B0D3255BFEF95601890AFD80709 ^" */
+      if (strlen(line) >= 42 && line[40] == ' ' && line[41] == '^')
+      {
+        line[40] = '\0';
+        nsrlhashes[hashcount] = strdup(line);
+        hashcount++;
+      }
+    }
+    fclose (infile);
+
+    fprintf (stderr, "hash count: %d\n", hashcount);
+
+    /* Open byte####.dat file that match the order of the hashes */
+    for (idx = 0; idx < hashcount; idx++)
+    {
+      snprintf (path, sizeof(path), "%s/byte%04d.dat", nsrldir, idx);
+      if (!(infile = fopen(path, "r")))
+      {
+        printf ("Error opening %s: %s\n", path, strerror(errno));
+        return 1;
+      }
+
+      /* Read file contents into buffer */
+      fstat (fileno(infile), &sb);
+      data = (char *) malloc ((size_t)sb.st_size);
+      fread (data, (size_t)sb.st_size, 1, infile);
+      fclose (infile);
+
+      printf ("File: %s\n", path);
+      failurecount += testhash (data, (size_t)sb.st_size, nsrlhashes[idx]);
+
+      free (data);
+    }
+  } /* Done with NSRL test vectors */
+
+
+  printf ("Failures: %d\n", failurecount);
+
+  return failurecount;
+}
